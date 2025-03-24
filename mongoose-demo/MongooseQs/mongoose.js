81a303def2ba7ac56771e15ff113cafb9cd64ef3
Sahil-Rawat-001 <rawatsahilwork001@gmail.com>
2025-03-24 10:37:11 +0530

    add mongoose.js where we learn about mongoose and its working with node.js

--- mongoose-demo/MongooseQs/mongoose.js
+++ mongoose-demo/MongooseQs/mongoose.js
@@ -3,25 +3,83 @@ import mongoose from 'mongoose';
 import Blog from './schema.js';
 
 
+
+
 // connecting mongoose
 mongoose.connect('mongodb://127.0.0.1:27017/BlogDatabase')
 .then( () => console.log('Mongodb Connected....') )
 .catch( (err) => console.log(`Error: ${err}`) );
 
-// create a new Blog post object
-const article = new Blog({
 
-    title:  "The World",
-    author: "Kite Doe",
-    isPublished: true,
-});
+// we define each field and its data type. Permitted types are:
+// String
+// Number
+// Date
+// Buffer
+// Boolean
+// Mixed
+// ObjectId
+// Array
+// Decimal128
+// Map
+
+
+
+// ******************** Insertion Method one *******************
+
+
+// create a new Blog post object
+// const article = new Blog({
+
+//     title:  "The World",
+//     author: "Kite Doe",
+//     isPublished: true,
+// });
 
 
 // Insert the article in mongodb 
-await article.save();
+// await article.save(); // Insertion method one
 
 
 // find a single blog post
-const firstArticle = await Blog.findOne({});
-console.log(firstArticle);
+// const firstArticle = await Blog.findOne({});
+// console.log(firstArticle);
 
+
+// ************************ Insertion method two ********************
+
+// create a new blog post and insert it into database
+
+// const article = await Blog.create({
+
+//     title: "Mountains & Goats",
+//     author: "Ram Singh Shekhawat",
+//     isPublished: false,
+// });
+
+// console.log(article);
+
+
+
+
+// *************** Updating Data ***************************
+
+// article.isPublished = true;
+// await article.save();
+
+// console.log(article);
+
+
+// *************** Finding Data **************************
+
+
+// const resultArticle = await Blog.findById('67dffca12fb8189b7d84d006', 'title isPublished').exec();
+// console.log(resultArticle);
+
+
+
+// **************** Deleting Data ************************
+
+// const blog = await Blog.deleteOne({author: 'Ram Singh Shekhawat'});
+const blog = await Blog.deleteMany({author: 'David Mcarthy'});
+console.log(blog);
