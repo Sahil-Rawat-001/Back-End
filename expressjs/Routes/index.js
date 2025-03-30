4dceb000cca8b3dd754f755c9f44424d4a82bdd8
Sahil-Rawat-001 <rawatsahilwork001@gmail.com>
2025-03-30 15:53:43 +0530

    learn about middle wares in same Routes/home,login and index.js file

--- expressjs/Routes/index.js
+++ expressjs/Routes/index.js
@@ -1,23 +1,39 @@
-const express = require('express');
-const loginRoute = require('./login.js');
-const homeRoute = require('./home.js');
+const express = require("express");
+const loginRoute = require("./login.js");
+const homeRoute = require("./home.js");
 const app = express();
+const fs = require("fs");
 
-app.use('/', loginRoute);
-app.use('/home', homeRoute);
+// middleware function
+const logRequests = (req, res, next) => {
+    
+  const logEntry = `Request made to ${req.url} on ${ new Date().toISOString().replace("T", " ").split(".")[0]} \n`;
 
-app.get('/', (req,res) => {
-    try{
-        return res.status(200).send('Hey, You are in Home Page');
-    } catch (error){
-        console.log(`Error: ${error}`);
-        res.status(400).json({err: 'Not found'});
-    }
-})
+  // usign fs.appendFile() method
+  fs.appendFile("./expressjs/Routes/log.txt", logEntry, (err) => {
+    if (err){
+     console.log(`Error: ${err.message}`);
+     return next();
+    } 
+  });
+
+  console.log(`log saved successfully!`);
+  next();
+};
+
+// Middle ware
+app.use(logRequests);
+app.use("/", loginRoute);
+app.use("/home", homeRoute);
+
+
+
+app.get("/", (req, res) => {
+    return res.status(200).send("Hey, You are in Home Page");
+});
 
 
 app.listen(8080, (err) => {
-
-    if(err) console.log(`Error: ${err}`);
-    console.log('Server started at port 8080');
+  if (err) console.log(`Error: ${err}`);
+  console.log("Server started at port 8080");
 });
